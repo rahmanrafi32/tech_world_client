0dc97eb235c66f3a89aa7a1ce8e0e187ac47bcb7
Md Minhazur Rahman Rafi <rafi.rahman03@yahoo.com>
2023-07-28 01:07:43 +0600

    added layout in homepage.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,10 @@ import {ReactElement} from "react";
 import RootLayout from "@/components/layouts/RootLayout";
 
 export default function Home() {
-    return (<div>
-        <h1>This is home</h1>
-    </div>)
+    return (
+        <div>
+            <h1>This is home</h1>
+        </div>)
 }
 
 Home.getLayout = function getLayout(page: ReactElement) {
