45d702240b3b438bec7d8fe59a4fd81512e1b115
Md Minhazur Rahman Rafi <rafi.rahman03@yahoo.com>
2023-07-28 01:06:25 +0600

    root layout created

---
+++ src/components/layouts/RootLayout.tsx
@@ -0,0 +1,18 @@
+import {ReactNode} from "react";
+import Navbar from "@/components/UI/Navbar";
+import Footer from "@/components/UI/Footer";
+
+type IProps = {
+    children: ReactNode
+}
+const RootLayout = ({children}: IProps) => {
+    return (
+        <>
+            <Navbar/>
+            <div className={'h-screen'}>{children}</div>
+            <Footer/>
+        </>
+    );
+};
+
+export default RootLayout;
