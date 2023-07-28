83b9737d20c2380a056c418c48c5d55ef6bad7f7
Md Minhazur Rahman Rafi <rafi.rahman03@yahoo.com>
2023-07-28 19:12:46 +0600

    condition on showing select button

--- src/components/UI/CategoryCards.tsx
+++ src/components/UI/CategoryCards.tsx
@@ -1,15 +1,19 @@
 import Link from "next/link";
 
 type IProps = {
-    featuredCategories: string[]
+    featuredCategories: string[],
+    showButton: boolean
 }
-const CategoryCards = ({featuredCategories}: IProps) => {
+const CategoryCards = ({featuredCategories, showButton}: IProps) => {
     return (
         featuredCategories.map((category, index) =>
             <div key={index} className="card bg-base-100 shadow-md lg:w-46 mx-2 mt-5 text-center hover:bg-slate-100">
                 <Link href={`/categories/${category}`}>
                     <div className="card-body">
                         <h2 className="text-xl font-semibold">{category}</h2>
+                        {
+                            showButton ? <button className={'btn mt-3'}>Select</button> : null
+                        }
                     </div>
                 </Link>
             </div>)
