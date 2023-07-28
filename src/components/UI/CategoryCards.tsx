f19904297267b549c5bffc9de416cdf8f280b379
Md Minhazur Rahman Rafi <rafi.rahman03@yahoo.com>
2023-07-28 14:10:32 +0600

    Link added to categorise component.

--- src/components/UI/CategoryCards.tsx
+++ src/components/UI/CategoryCards.tsx
@@ -1,15 +1,18 @@
-import {featuredCategories} from "@/data";
+import Link from "next/link";
 
 type IProps = {
     featuredCategories: string[]
 }
 const CategoryCards = ({featuredCategories}: IProps) => {
     return (
-       featuredCategories.map((category,index)=> <div key={index} className="card bg-base-100 shadow-xl lg:w-46 mx-2 mt-5 text-center">
-           <div className="card-body">
-               <h2 className="text-xl font-semibold">{category}</h2>
-           </div>
-       </div>)
+        featuredCategories.map((category, index) =>
+            <div key={index} className="card bg-base-100 shadow-md lg:w-46 mx-2 mt-5 text-center hover:bg-slate-100">
+                <Link href={`/categories/${category}`}>
+                    <div className="card-body">
+                        <h2 className="text-xl font-semibold">{category}</h2>
+                    </div>
+                </Link>
+            </div>)
     );
 };
 
