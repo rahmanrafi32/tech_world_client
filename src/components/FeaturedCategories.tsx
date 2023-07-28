9228c927bd6c3a23378407c257046eac05f8eb05
Md Minhazur Rahman Rafi <rafi.rahman03@yahoo.com>
2023-07-28 14:09:52 +0600

    some css properties changed.

--- src/components/FeaturedCategories.tsx
+++ src/components/FeaturedCategories.tsx
@@ -3,9 +3,9 @@ import {featuredCategories} from "@/data";
 
 const FeaturedCategories = () => {
     return (
-        <div className={'flex flex-col items-center mt-28'}>
-            <h1 className={'lg:text-5xl'}>Featured Category</h1>
-            <p className={'lg:text-xl mt-3 mb-10'}>Get Your Desired Product from Featured Category!</p>
+        <div className={'flex flex-col items-center mt-28 mb-10'}>
+            <h1 className={'text-3xl lg:text-5xl'}>Featured Category</h1>
+            <p className={'text-md lg:text-xl mt-3 mb-10 text-center'}>Get Your Desired Product from Featured Category!</p>
             <div className="grid lg:grid-cols-3 sm:grid-cols-1 gap-3">
                 <CategoryCards featuredCategories={featuredCategories}/>
             </div>
