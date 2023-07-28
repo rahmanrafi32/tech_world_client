49f9baf6b015c2f905826c4407e4141865d68f17
Md Minhazur Rahman Rafi <rafi.rahman03@yahoo.com>
2023-07-28 14:08:10 +0600

    changed img to next/Image

--- src/components/UI/ProductCards.tsx
+++ src/components/UI/ProductCards.tsx
@@ -1,4 +1,5 @@
 import {FeaturedProduct} from "@/data";
+import Image from "next/image";
 
 type IProps = {
     featuredProducts: FeaturedProduct[]
@@ -8,13 +9,16 @@ const ProductCards = ({featuredProducts}: IProps) => {
     return (
         <div className={'grid lg:grid-cols-3 gap-4 sm:grid-cols-1'}>
             {
-                featuredProducts.slice(0,6).map(product =>
+                featuredProducts.slice(0, 6).map(product =>
                     <div className="card bg-base-100 shadow-xl lg:w-96 mx-2" key={product.productName}>
-                        <figure><img
-                            src={product.image}
-                            alt={product.productName}
-                            className={'p-4'}
-                        />
+                        <figure>
+                            <Image
+                                src={product.image}
+                                alt={product.productName}
+                                className={'p-4'}
+                                width={200}
+                                height={200}
+                            />
                         </figure>
                         <div className="card-body">
                             <h2 className="card-title">{product.productName}</h2>
