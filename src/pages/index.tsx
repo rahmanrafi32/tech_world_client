963e242580d16a3a4c05e3ef9fb013ffa0155ce1
Md Minhazur Rahman Rafi <rafi.rahman03@yahoo.com>
2023-07-28 17:09:54 +0600

    called api in home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,20 @@ import RootLayout from "@/components/layouts/RootLayout";
 import Banner from "@/components/Banner";
 import AllFeaturedProducts from "@/components/AllFaeturedProducts";
 import FeaturedCategories from "@/components/FeaturedCategories";
+import {GetStaticProps} from "next";
+import {FeaturedProduct} from "@/data";
 
-export default function Home() {
+type IProps = {
+    allProducts: FeaturedProduct[]
+}
+const Home = ({allProducts}: IProps) => {
     return (
         <div className={'container mx-auto'}>
             <div className={'flex justify-center mt-5'}>
                 <Banner/>
             </div>
             <div className={'flex flex-col items-center mb-4'}>
-                <AllFeaturedProducts/>
+                <AllFeaturedProducts featuredProducts={allProducts}/>
                 <FeaturedCategories/>
             </div>
         </div>
@@ -21,3 +26,15 @@ export default function Home() {
 Home.getLayout = function getLayout(page: ReactElement) {
     return (<RootLayout>{page}</RootLayout>)
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+    const products = await fetch('http://localhost:8080/api/v1/products');
+    const allProducts = await products.json()
+    return {
+        props: {
+            allProducts: allProducts.data
+        },
+    };
+}
+
+export default Home;
