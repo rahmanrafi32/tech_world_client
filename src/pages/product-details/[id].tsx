65cae73736053272b6c08c25530e767f39d17838
Md Minhazur Rahman Rafi <rafi.rahman03@yahoo.com>
2023-07-29 13:31:02 +0600

    added product full description

--- src/pages/product-details/[id].tsx
+++ src/pages/product-details/[id].tsx
@@ -1,10 +1,14 @@
 import {ReactElement} from "react";
 import RootLayout from "@/components/layouts/RootLayout";
 import {GetStaticPaths, GetStaticProps} from "next";
-import {FeaturedProduct} from "@/data";
+import {FeaturedProduct} from "@/types";
 import Image from "next/image";
+import {BiSolidUser} from 'react-icons/bi'
 
-const ProductDetails = ({productsById}:any) => {
+type IProps = {
+    productsById: FeaturedProduct
+}
+const ProductDetails = ({productsById}: IProps) => {
     return (
         <div className={'container mx-auto h-full w-screen flex justify-center mt-28'}>
             <div className={'grid grid-cols-1 md:grid-cols-2 gap-5'}>
@@ -16,19 +20,31 @@ const ProductDetails = ({productsById}:any) => {
                     <h1 className={'text-xl mt-2'}>Category: {productsById.category}</h1>
                     <h2 className={'text-xl'}>{productsById.status}</h2>
                     <h2 className={'text-2xl'}>Description</h2>
-
+                    <h2>{productsById.description}</h2>
                     <h2 className={'text-2xl mt-8'}>Key Features</h2>
-                    <h2 className={'text-xl mt-3'}>Model: PRO MP223</h2>
-                    <h2 className={'text-xl'}>FHD (1920 x 1080)</h2>
-                    <h2 className={'text-xl'}>VA, 100Hz, 1ms</h2>
-                    <h2 className={'text-xl'}>1x HDMI, 1x VGA, 1x Audio Out</h2>
-                    <h2 className={'text-xl'}> Anti-Flicker Technology</h2>
+                    <h2 className={'text-xl mt-3'}>Brand: {productsById?.features?.brand}</h2>
+                    <h2 className={'text-xl'}>Model: {productsById?.features?.model}</h2>
+                    <h2 className={'text-xl'}>Specification: {productsById?.features?.Specification}</h2>
+                    <h2 className={'text-xl'}>Type: {productsById?.features?.Type}</h2>
+                    <h2 className={'text-xl'}>Average Rating: {productsById?.avgRating}</h2>
+                    <h2 className={'text-xl'}>Individual Rating: {productsById?.individualRating}</h2>
+
 
                     <h2 className={'text-xl mt-3'}>Price: {productsById.price}</h2>
                     <button className={'btn btn-primary mt-10'}>Buy Now</button>
                 </div>
-                <div>
-                    <h1 className={'text-3xl'}>Reviews</h1>
+                <div className={'px-4'}>
+                    <h1 className={'text-3xl'}>Reviews :</h1>
+
+                    <div className={'mt-10 flex flex-col gap-4 mb-20 '}>
+                        {
+                            productsById?.reviews?.map((review: string, index: number) =>
+                                <div key={index} className={'flex items-center mt-2 '}>
+                                    <BiSolidUser className={'text-4xl mr-4'}/>
+                                    <h1 className={'text-xl'}>{review}</h1>
+                                </div>)
+                        }
+                    </div>
                 </div>
             </div>
         </div>
