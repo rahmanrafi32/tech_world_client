2d3e51bbf763e59b08a5f615a2594f00855c1ba1
Md Minhazur Rahman Rafi <rafi.rahman03@yahoo.com>
2023-07-29 13:29:37 +0600

    changed import.

--- src/components/UI/ProductCards.tsx
+++ src/components/UI/ProductCards.tsx
@@ -1,4 +1,4 @@
-import {FeaturedProduct} from "@/data";
+import {FeaturedProduct} from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 import {useRouter} from "next/router";
